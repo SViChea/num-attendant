f79d98565d2e3f24d56e934f19fadd3819777c42
ViChea <ssovicheasok@gmail.com>
2025-07-25 16:53:34 +0700

    Firebase Configuration Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:num_attendant/welcome_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
+  
 }
 
 // The root of the application
